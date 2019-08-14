1d3f0e606fd98e6fff35764841beb1ecbc675d48
guwenchang <1379864599@qq.com>
2019-08-14 20:11:42 +0800

    update

--- src/main/java/com/example/leetcode/sort/SortTest.java
+++ src/main/java/com/example/leetcode/sort/SortTest.java
@@ -10,7 +10,7 @@ public class SortTest {
 
     public static void main(String[] args) {
         int[] nums = new int[]{2,3,5,1,6};
-        Sort sort = new QuickSort();
+        Sort sort = new HeapSort();
         nums = sort.sort(nums);
         System.out.println(Arrays.toString(nums));
     }
